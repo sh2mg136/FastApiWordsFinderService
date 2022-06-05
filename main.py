80196b4d134d5be854b3c2e8d6ea1adbdbfdadd1
maxx <sh2mg@136@gmail.com>
2022-06-05 19:09:33 +0300

    test local

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import logging.config
 logging.config.fileConfig(fname='logging.conf', disable_existing_loggers=False)
 logger = logging.getLogger(__name__)
 
+
 # F:
 # cd python\FastApiTestProject
 # uvicorn main:app --port 8000
